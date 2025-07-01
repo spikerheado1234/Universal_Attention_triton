Cumsum fixed, now debugging the softmax-matmul part, where my unit testing works fine...

Universal_Attention/triton/debug.py
@@ -1,28 +1,31 @@
 import torch
 import triton
 import triton.language as tl
+import numpy as np
+import inspect
 
-configs = [
-    triton.Config({}, num_stages=stages, num_warps=warps) \
-    for stages in [2, 3, 4]\
-    for warps in [2, 4, 8]\
-]
+# configs = [
+#     triton.Config({}, num_stages=stages, num_warps=warps) \
+#     for stages in [2, 3, 4, 5]\
+#     for warps in [2, 4, 8, 16]\
+# ]
 
 '''
 ######################################
 #     Forward Kernel & Interface     #
 ######################################
 '''
-@triton.autotune(
-    configs=configs,
-    key=['s', 'd'],
-)
+# @triton.autotune(
+#     configs=configs,
+#     key=['s', 'd'],
+# )
 @triton.jit
 def _universal_attention_fwd_kernel(
     # Pointers to matrices
     q_ptr, k_ptr, v_ptr, src_ptr, dest_ptr, output_ptr, 
     spinlock_ptr, semaphore_ptr, sense_rev_ptr, 
     cum_cache_ptr, max_cache_ptr, sum_cache_ptr, 
+    debug_ptr,
     # Matrix dimensions
     b, n_kv, rep, s, d, 
     # Strides
@@ -38,6 +41,7 @@ def _universal_attention_fwd_kernel(
     stride_cum_b, stride_cum_n_kv, stride_cum_s,
     stride_max_b, stride_max_n_kv, stride_max_s, stride_max_c, 
     stride_sum_b, stride_sum_n_kv, stride_sum_s, stride_sum_c, 
+    stride_debug_b, stride_debug_n_kv, stride_debug_s, stride_debug_s1,
     # Meta-parameters
     BLOCK_C: tl.constexpr, BLOCK_D: tl.constexpr, 
     C_BLOCK: tl.constexpr, D_BLOCK: tl.constexpr,
@@ -121,7 +125,6 @@ def _universal_attention_fwd_kernel(
     affinity = tl.cumsum(affinity, axis=1) # local cumsum
 
     # Make sure the sum is computed sequentially
-    # while tl.load(spinlock_ptr, mask=True, other=0) < pid_n:
     while tl.atomic_add(spinlock_ptr, 0, sem="acquire") < pid_n:
         pass
 
@@ -132,22 +135,27 @@ def _universal_attention_fwd_kernel(
             mask=offs_m < s, 
             other=0.0
         ) 
-    curr_sum += prev_sum
     tl.store(
         cum_cache_ptr + offs_m * stride_cum_s, 
-        curr_sum, 
+        curr_sum + prev_sum, 
         mask=offs_m < s
     )
+
     tl.atomic_add(spinlock_ptr, 1, sem="release")
 
-    affinity += prev_sum[:, None]
+    affinity = affinity + prev_sum[:, None]
 
     # .masked_fill(mask.tril(-1), -1e12)
     # tril_mask = (offs_m[:, None] > offs_n[None, :])
     affinity = tl.where((offs_m[:, None] > offs_n[None, :]), -1e12, affinity)
 
     # Affinity matrix completed here
-
+    # debug_ptr += pid_b * stride_debug_b + pid_n_kv * stride_debug_n_kv
+    # tl.store(
+    #     debug_ptr + offs_m[:, None] * stride_debug_s + offs_n[None, :] * stride_debug_s1,
+    #     affinity,
+    #     mask=(offs_m[:, None] < s) & (offs_n[None, :] < s),
+    # )
     # k_.unsqueeze(2).matmul(xq.transpose(-1,-2)).add(affinity.unsqueeze(2))
     q_ptr += pid_b * stride_q_b + pid_n_kv * rep * stride_q_n_rep
     v_ptr += pid_b * stride_v_b + pid_n_kv * stride_v_n_kv
@@ -204,10 +212,13 @@ def _universal_attention_fwd_kernel(
             mask=(offs_m < s)
         )
 
-        tl.atomic_add(semaphore_ptr, 1, sem="release")
+        if pid_n == 0:
+            tl.atomic_xchg(semaphore_ptr, 0)
+        while tl.load(semaphore_ptr, mask=True, other=0) > 0:
+            pass
+        tl.atomic_add(semaphore_ptr, 1)
         # Don't use atomic read here, or it will prevent other atomic operations
-        # while tl.load(semaphore_ptr, mask=True, other=0) < C_BLOCK:
-        while tl.atomic_add(semaphore_ptr, 0, sem="acquire") < C_BLOCK:
+        while tl.load(semaphore_ptr, mask=True, other=0) < C_BLOCK:
             pass
 
         localmax_mat = tl.load(
@@ -230,10 +241,8 @@ def _universal_attention_fwd_kernel(
 
         # qk_softmax @ V
         if pid_n == 0:
-            tl.atomic_xchg(semaphore_ptr, 0, sem="release")
-        
-        # while tl.load(semaphore_ptr, mask=True, other=0) > 0:
-        while tl.atomic_add(semaphore_ptr, 0, sem="acquire") > 0:
+            tl.atomic_xchg(semaphore_ptr, 0)
+        while tl.load(semaphore_ptr, mask=True, other=0) > 0:
             pass
 
         for idx in range(0, C_BLOCK + D_BLOCK - 1):
@@ -241,11 +250,10 @@ def _universal_attention_fwd_kernel(
             local_sense = tl.load(sense_rev_ptr, mask=True, other=0)
             num_proc = tl.atomic_add(semaphore_ptr, 1)
             if num_proc == C_BLOCK - 1:
-                tl.atomic_xchg(semaphore_ptr, 0, sem="release")
-                tl.atomic_xchg(sense_rev_ptr, 1 - local_sense, sem="release")
+                tl.atomic_xchg(semaphore_ptr, 0)
+                tl.atomic_xchg(sense_rev_ptr, 1 - local_sense)
             else:
-                while tl.atomic_add(sense_rev_ptr, 0, sem="acquire") == local_sense:
-                # while tl.load(sense_rev_ptr, mask=True, other=0) == local_sense:
+                while tl.load(sense_rev_ptr, mask=True, other=0) == local_sense:
                     pass
 
             d_idx = idx - pid_n 
@@ -278,8 +286,8 @@ def _universal_attention_fwd(q, k, v, src, dest):
     device = q.device
     dtype_flag = tl.float16 if q.dtype == torch.float16 else tl.float32
     
-    BLOCK_C = 64
-    BLOCK_D = 64
+    BLOCK_C = 16
+    BLOCK_D = 16
     C_BLOCK = triton.cdiv(s, BLOCK_C)
     D_BLOCK = triton.cdiv(d, BLOCK_D)
 
@@ -291,6 +299,8 @@ def _universal_attention_fwd(q, k, v, src, dest):
     cum_cache = torch.empty((b, n_kv, s), device=device, dtype=torch.float32)
     max_cache = torch.empty((b, n_kv, s, C_BLOCK), device=device, dtype=torch.float32)
     sum_cache = torch.empty((b, n_kv, s, C_BLOCK), device=device, dtype=torch.float32)
+
+    debug = torch.empty((b, n_kv, s, s), device=device, dtype=torch.float32)
     
     # grid = lambda META: (b * n_kv, triton.cdiv(s, META['BLOCK_C']), triton.cdiv(s, META['BLOCK_C']))
     grid = (b * n_kv, C_BLOCK, C_BLOCK)
@@ -299,6 +309,7 @@ def _universal_attention_fwd(q, k, v, src, dest):
         q, k, v, src, dest, output, 
         spinlock, semaphore, sense_rev, 
         cum_cache, max_cache, sum_cache, 
+        debug,
         b, n_kv, rep, s, d, 
         q.stride(0), q.stride(1), q.stride(2), q.stride(3),                                 # (b, n, s, d)
         k.stride(0), k.stride(1), k.stride(2), k.stride(3),                                 # (b, n_kv, s, d)
@@ -312,12 +323,13 @@ def _universal_attention_fwd(q, k, v, src, dest):
         cum_cache.stride(0), cum_cache.stride(1), cum_cache.stride(2),                      # (b, n_kv, s)
         max_cache.stride(0), max_cache.stride(1), max_cache.stride(2), max_cache.stride(3), # (b, n_kv, s, C_BLOCK)
         sum_cache.stride(0), sum_cache.stride(1), sum_cache.stride(2), sum_cache.stride(3), # (b, n_kv, s, C_BLOCK)
+        debug.stride(0), debug.stride(1), debug.stride(2), debug.stride(3),                 # For debugging
         BLOCK_C=BLOCK_C, BLOCK_D=BLOCK_D, 
         C_BLOCK=C_BLOCK, D_BLOCK=D_BLOCK,
         DTYPE=dtype_flag,
     )
     del spinlock, semaphore, sense_rev, cum_cache, max_cache, sum_cache
-    return output
+    return output#, debug
 
 '''
 #################################
@@ -327,7 +339,7 @@ def _universal_attention_fwd(q, k, v, src, dest):
 def universal_attention_forward(q, k, v, src, dest):
     b, h, l, d = k.shape
     _, nh, _, _ = q.shape
-    c = 64
+    c = 16
     n = l // c
     r = nh // h
 
@@ -345,6 +357,9 @@ def universal_attention_forward(q, k, v, src, dest):
     denom = torch.empty(b,h,r,l,n, dtype=xq.dtype, device=xq.device)
     static_src = static_src.pow(1/3)
     static_dest = static_dest.pow(1/3)
+
+    affinity_collect = torch.empty(b,h,c,l,n, dtype=xq.dtype, device=xq.device)
+
     kt = kc.view(b,h,l,d).transpose(-2,-1)  # b h d l
     for i in range(n):
         k_ = kc[:,:,i]  # b h c d
@@ -358,6 +373,7 @@ def universal_attention_forward(q, k, v, src, dest):
         affinity = affinity.triu(i*c+1).cumsum(3)  # Accumulate decay with causal masking
         affinity = affinity.masked_fill(mask.tril(i*c-1), -1e12)  # Re-mask, with 1s on diagonal
 
+        # affinity_collect[...,i] = affinity
         # Perform actual attention operation
         score = k_.unsqueeze(2).matmul(xq.transpose(-1,-2)).add(affinity.unsqueeze(2))  # b h r c l
         denom_ = score.logsumexp(dim=-2)  # b h r l
@@ -366,6 +382,7 @@ def universal_attention_forward(q, k, v, src, dest):
         out[...,i] = out_
         denom[...,i] = denom_
     output = out.mul(denom.softmax(dim=-1).unsqueeze(-2)).sum(-1)
+    # return affinity_collect
     return output.reshape(b, nh, l, d)
 
 '''
@@ -400,14 +417,37 @@ def _universal_attention_bwd(kc, vc, xq, static_src, static_dest, dout, ddenom):
     return None, None, None, None, None
 
 if __name__ == "__main__":
-    b, n_kv, rep, s, d = 2, 4, 4, 4096, 4096
+    b, n_kv, rep, s, d = 2, 4, 8, 64, 64
     q = torch.rand((b, n_kv * rep, s, d), device='cuda', dtype=torch.float32)
     k = torch.rand((b, n_kv, s, d), device='cuda', dtype=torch.float32)
     v = torch.rand((b, n_kv, s, d), device='cuda', dtype=torch.float32)
     src = torch.rand((b, n_kv, s), device='cuda', dtype=torch.float32)
     dest = torch.rand((b, n_kv, s), device='cuda', dtype=torch.float32)
 
+    # _, affinity = _universal_attention_fwd(q, k, v, src, dest)
+    # affinity_ref = universal_attention_forward(q, k, v, src, dest)
+    # affinity_ref = torch.permute(affinity_ref, (0, 1, 4, 2, 3)).reshape(b, n_kv, s, s)
     output = _universal_attention_fwd(q, k, v, src, dest)
     output_ref = universal_attention_forward(q, k, v, src, dest)
+    # affinity_ref = torch.permute(affinity_ref, (0, 1, 4, 2, 3)).reshape(b, n_kv, s, s)
+
+    count = 0
+    for arr in (output, output_ref):
+        arr = arr.cpu().numpy().reshape(-1, arr.shape[-1])
+        # print(
+        #     np.array2string(
+        #         affinity.cpu().numpy(),
+        #         precision=2,
+        #         suppress_small=True,
+        #         max_line_width=1200
+        #     )
+        # )
+        np.savetxt(f"output{count}.csv", arr, delimiter=",", fmt="%.6f")   
+        count += 1
+
+    # output = _universal_attention_fwd(q, k, v, src, dest)
+    # output_ref = universal_attention_forward(q, k, v, src, dest)
+    diff = (output - output_ref).abs()
+    print("Max abs diff:", diff.max().item())
 
     torch.testing.assert_close(output, output_ref)