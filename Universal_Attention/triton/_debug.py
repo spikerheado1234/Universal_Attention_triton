fix causal masking for forward pass

Universal_Attention/triton/_debug.py
@@ -10,7 +10,7 @@ def sdpa_torch(q, k, v, causal=False):
     """
     qk = torch.einsum('bnqh, bnkh -> bnqk', q, k)/torch.full((q.shape[0],q.shape[1],q.shape[2],k.shape[2]), sqrt(q.shape[-1])).to(q.device) ## S
     if causal:
-        qk += torch.triu(torch.full(qk.shape, -1e-9))
+        qk += torch.triu(torch.full(qk.shape, -1e6), diagonal=1).to(q.device)
     attn = torch.nn.functional.softmax(qk, dim=-1,dtype=torch.float32) ## P
     o = torch.einsum('bnqk, bnkh -> bnqh', attn.to(dtype=q.dtype), v) ## O
     return o, attn.to(dtype=q.dtype)
@@ -30,7 +30,6 @@ def sdpa_torch_bwd(attn, k, v, q, o, incoming_gradients):
     dqk = (attn * dattn) - (torch.unsqueeze(d, dim=-1) * attn)  ## Check the correctness of this, may be incorrect.
     print(f'dqk: {dqk.sum()}')
     print(f'attn: {attn.sum()}')
-    print(f'attn: {attn}')
 
     ## Finally, we compute dq and dk. ##
     dq = torch.einsum('bnqk, bnkt -> bnqt', dqk, k) / torch.full(q.shape, sqrt(q.shape[-1])).to(q.device).to(dtype=q.dtype)
@@ -52,7 +51,7 @@ def _debug_triton_fused_mhsa(q,k,v, backward=False, causal=False):
     do = torch.randn_like(q).to(q.device)
     triton_output = fn()
     if backward:
-        dq_sanity, dk_sanity, dv_sanity = sdpa_torch_bwd(attn, k_sanity, v_sanity, q_sanity, sdpa_output, do, causal)
+        dq_sanity, dk_sanity, dv_sanity = sdpa_torch_bwd(attn, k_sanity, v_sanity, q_sanity, sdpa_output, do)
         fn = lambda: triton_output.backward(do, retain_graph=True)
         fn()
         sdpa_output.backward(do, retain_graph=True)
@@ -75,13 +74,14 @@ if __name__ == '__main__':
     torch.manual_seed(0)
     BATCH=1
     H=1
-    N_CTX=128
+    N_CTX=32
     HEAD_DIM=16
+    causal = True
     device="cuda" if torch.cuda.is_available() else "cpu"
     provider = "triton" ## triton/flash.
     dtype=torch.float16
     q = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
     k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
     v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
-    _debug_triton_fused_mhsa(q,k,v, backward=True)
+    _debug_triton_fused_mhsa(q,k,v, backward=True, causal=causal)
 

Universal_Attention/triton/universal_attention_kernel_opt.py
@@ -40,7 +40,7 @@ def _attn_fwd_inner(acc, l_i, m_i, q,  #
                     offset_y, dtype: tl.constexpr, start_m, qk_scale,  #
                     BLOCK_M: tl.constexpr, HEAD_DIM: tl.constexpr, BLOCK_N: tl.constexpr,  #
                     STAGE: tl.constexpr, offs_m: tl.constexpr, offs_n: tl.constexpr,  #
-                    N_CTX: tl.constexpr):
+                    N_CTX: tl.constexpr, causal: tl.constexpr):
     # range of values handled by this stage
     if STAGE == 1:
         lo, hi = 0, start_m * BLOCK_M
@@ -50,8 +50,8 @@ def _attn_fwd_inner(acc, l_i, m_i, q,  #
     # causal = False
     else:
         lo, hi = 0, N_CTX
-    offsetk_y = offset_y + lo
-    offsetv_y = offset_y + lo
+    offsetk_y = offset_y + lo * HEAD_DIM
+    offsetv_y = offset_y + lo * HEAD_DIM
     # loop over k, v and update accumulator
     for start_n in tl.range(lo, hi, BLOCK_N):
         start_n = tl.multiple_of(start_n, BLOCK_N)
@@ -71,6 +71,7 @@ def _attn_fwd_inner(acc, l_i, m_i, q,  #
             m_ij = tl.maximum(m_i, tl.max(qk, 1))
             #qk = qk * qk_scale - m_ij[:, None]
             qk = qk - m_ij[:, None]
+
         p = tl.math.exp(qk)
         # -- compute correction factor
         #alpha = tl.math.exp2(m_i - m_ij)
@@ -105,6 +106,7 @@ def _attn_fwd(sm_scale, M,  #
               FP8_OUTPUT: tl.constexpr,  #
               STAGE: tl.constexpr,  #
               warp_specialize: tl.constexpr,  #
+              causal: tl.constexpr,
               ):
     dtype = tl.float8e5 if FP8_OUTPUT else tl.float16
     tl.static_assert(BLOCK_N <= HEAD_DIM)
@@ -138,14 +140,14 @@ def _attn_fwd(sm_scale, M,  #
                                         desc_k, desc_v,  #
                                         offset_y, dtype, start_m, qk_scale,  #
                                         BLOCK_M, HEAD_DIM, BLOCK_N,  #
-                                        4 - STAGE, offs_m, offs_n, N_CTX)
+                                        4 - STAGE, offs_m, offs_n, N_CTX, causal)
     # stage 2: on-band
     if STAGE & 2:
         acc, l_i, m_i = _attn_fwd_inner(acc, l_i, m_i, q,  #
                                         desc_k, desc_v,  #
                                         offset_y, dtype, start_m, qk_scale,  #
                                         BLOCK_M, HEAD_DIM, BLOCK_N,  #
-                                        2, offs_m, offs_n, N_CTX)
+                                        2, offs_m, offs_n, N_CTX, causal)
     # epilogue
     m_i += tl.math.log(l_i)
     acc = acc / l_i[:, None]
@@ -423,6 +425,7 @@ class _attention(torch.autograd.Function):
             FP8_OUTPUT=q.dtype == torch.float8_e5m2,  #
             STAGE=stage,  #
             warp_specialize=warp_specialize,  #
+            causal=causal,
             **extra_kern_args)
 
         ctx.save_for_backward(q, k, v, o, M)
@@ -485,7 +488,7 @@ if __name__ == '__main__':
     mode="fwd"
     BATCH=2
     H=32
-    N_CTX=4096
+    N_CTX=16
     HEAD_DIM=128
     device="cuda" if torch.cuda.is_available() else "cpu"
     provider = "triton" ## triton/flash.