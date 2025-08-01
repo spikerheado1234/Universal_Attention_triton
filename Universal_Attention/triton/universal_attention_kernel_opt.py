8fa4ec41df80a9475963a73921736476574822c1
spikerheado1234 <ahangupta.96@gmail.com>
2025-08-01 15:34:17 -0700

    remove extraneous comments

--- Universal_Attention/triton/universal_attention_kernel_opt.py
+++ Universal_Attention/triton/universal_attention_kernel_opt.py
@@ -356,7 +356,6 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     dk_ptrs = DK + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
     tl.store(dk_ptrs, dk.to(tl.float16), mask=offs_n[:, None] < N_CTX)
 
-    # ----- BEGIN dQ REPLACEMENT -----
     # THIS BLOCK DOES DQ:
     start_m = pid * BLOCK_M2
     offs_m = start_m + tl.arange(0, BLOCK_M2)
@@ -391,52 +390,6 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     dq_ptrs = DQ + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
     dq *= sm_scale
     tl.store(dq_ptrs, dq, mask=offs_m[:, None] < N_CTX)
-    # ----- END dQ REPLACEMENT -----
-
-    ## THis is the old code-path. ##
-    #start_m = pid * BLOCK_M2
-    #end_n = start_m + BLOCK_M2
-
-    #MASK_BLOCK_N2: tl.constexpr = BLOCK_N2 // BLK_SLICE_FACTOR
-    #offs_m = start_m + tl.arange(0, BLOCK_M2)
-
-    #q = tl.load(Q + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d, mask=offs_m[:, None] < N_CTX)
-    #dq = tl.zeros([BLOCK_M2, HEAD_DIM], dtype=tl.float32)
-    #do = tl.load(DO + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d, mask=offs_m[:, None] < N_CTX)
-
-    #m = tl.load(M + offs_m, mask=offs_m < N_CTX)
-    #m = m[:, None]
-
-    ## Compute dQ for masked (diagonal) blocks.
-    ## NOTE: This code scans each row of QK^T backward (from right to left,
-    ## but inside each call to _attn_bwd_dq, from left to right), but that's
-    ## not due to anything important.  I just wanted to reuse the loop
-    ## structure for dK & dV above as much as possible.
-    ### This is the old code, we keep in case we would like to revert to it. ##
-    #num_steps = BLOCK_M2 // MASK_BLOCK_N2
-    #dq = _attn_bwd_dq(dq, q, K, V,  #
-    #                  do, m, D,  #
-    #                  stride_tok, stride_d,  #
-    #                  H, N_CTX,  #
-    #                  BLOCK_M2, MASK_BLOCK_N2, HEAD_DIM,  #
-    #                  start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,  #
-    #                  MASK=True  #
-    #                  )
-    #end_n -= num_steps * MASK_BLOCK_N2
-    ## stage 2
-    #num_steps = end_n // BLOCK_N2
-    #dq = _attn_bwd_dq(dq, q, K, V,  #
-    #                  do, m, D,  #
-    #                  stride_tok, stride_d,  #
-    #                  H, N_CTX,  #
-    #                  BLOCK_M2, BLOCK_N2, HEAD_DIM,  #
-    #                  start_m, end_n - num_steps * BLOCK_N2, num_steps,  #
-    #                  MASK=False  #
-    #                  )
-    ## Write back dQ.
-    #dq_ptrs = DQ + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
-    #dq *= sm_scale
-    #tl.store(dq_ptrs, dq, mask=offs_m[:, None] < N_CTX)
 
 class _attention(torch.autograd.Function):
 
