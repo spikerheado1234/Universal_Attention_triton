need to fix causal masking, slightly buggy

Universal_Attention/triton/_debug.py
@@ -9,19 +9,27 @@ def sdpa_torch(q, k, v):
     Einsum variant of sdpa, to track intermediate tensor values.
     """
     qk = torch.einsum('bnqh, bnkh -> bnqk', q, k)/torch.full((q.shape[0],q.shape[1],q.shape[2],k.shape[2]), sqrt(q.shape[-1])).to(q.device) ## S
+    print(f'qk: {qk.sum()}')
     attn = torch.nn.functional.softmax(qk, dim=-1,dtype=torch.float32) ## P
     o = torch.einsum('bnqk, bnkh -> bnqh', attn.to(dtype=q.dtype), v) ## O
     return o, attn.to(dtype=q.dtype)
 
 def sdpa_torch_bwd(attn, k, v, q, o, incoming_gradients):
     ## Compute the gradient of v & attn ##
+    print(f'q: {q.sum()}')
+    print(f'k: {k.sum()}')
+    print(f'v: {v.sum()}')
     dv = torch.einsum('bnqk, bnqh -> bnkh', attn, incoming_gradients)
     dattn = torch.einsum('bnqh, bnkh -> bnqk', incoming_gradients, v)
 
     ## next, we compute the derivative of qk. We use the trick that: dqk = attn * dattn - row_sum(incoming_gradients * o) * attn. ##
     ##  here d = row_sum(incoming_gradients * o).
     d = torch.sum(incoming_gradients * o, axis=-1)  ## (b, n, s) -> Similar sized shape to logsumexp.
+    print(f'd: {d.sum()}')
     dqk = (attn * dattn) - (torch.unsqueeze(d, dim=-1) * attn)  ## Check the correctness of this, may be incorrect.
+    print(f'dqk: {dqk.sum()}')
+    print(f'attn: {attn.sum()}')
+    print(f'attn: {attn}')
 
     ## Finally, we compute dq and dk. ##
     dq = torch.einsum('bnqk, bnkt -> bnqt', dqk, k) / torch.full(q.shape, sqrt(q.shape[-1])).to(q.device).to(dtype=q.dtype)
@@ -39,24 +47,24 @@ def _debug_triton_fused_mhsa(q,k,v, backward=False):
     k_sanity = k.clone().detach().requires_grad_(True)
     v_sanity = v.clone().detach().requires_grad_(True)
     sm_scale = 1.3
-    causal=False
+    causal = True
     fn = lambda: attention(q, k, v, causal, sm_scale)
     do = torch.randn_like(q).to(q.device)
     triton_output = fn()
     if backward:
+        dq_sanity, dk_sanity, dv_sanity = sdpa_torch_bwd(attn, k_sanity, v_sanity, q_sanity, sdpa_output, do)
         fn = lambda: triton_output.backward(do, retain_graph=True)
         fn()
         sdpa_output.backward(do, retain_graph=True)
 
     print(f'outputs allclose: {torch.allclose(sdpa_output, triton_output, atol=1e-1, rtol=1e-1)}')
     if backward:
-        print(f'q_torch.grad: {q_torch.grad}')
-        print(f'q.grad: {q.grad}')
+        #print(f'q_torch.grad: {q_torch.grad}')
+        #print(f'q.grad: {q.grad}')
         print(f'dq allclose: {torch.allclose(q_torch.grad, q.grad, atol=1e-1, rtol=1e-1)}')
         print(f'dk allclose: {torch.allclose(k_torch.grad, k.grad, atol=1e-1, rtol=1e-1)}')
         print(f'dv allclose: {torch.allclose(v_torch.grad, v.grad, atol=1e-1, rtol=1e-1)}')
         print(f'------output sanity checking------')
-        dq_sanity, dk_sanity, dv_sanity = sdpa_torch_bwd(attn, k_sanity, v_sanity, q_sanity, sdpa_output, do)
         print(f'dq allclose: {torch.allclose(q_torch.grad, dq_sanity, atol=1e-1, rtol=1e-1)}')
         print(f'dk allclose: {torch.allclose(k_torch.grad, dk_sanity, atol=1e-1, rtol=1e-1)}')
         print(f'dv allclose: {torch.allclose(v_torch.grad, dv_sanity, atol=1e-1, rtol=1e-1)}')
@@ -67,7 +75,7 @@ if __name__ == '__main__':
     torch.manual_seed(0)
     BATCH=1
     H=1
-    N_CTX=16
+    N_CTX=128
     HEAD_DIM=16
     device="cuda" if torch.cuda.is_available() else "cpu"
     provider = "triton" ## triton/flash.

Universal_Attention/triton/universal_attention_kernel_opt.py
@@ -182,29 +182,27 @@ def _attn_bwd_dkdv(dk, dv,  #
                    BLOCK_N1: tl.constexpr,  #
                    HEAD_DIM: tl.constexpr,  #
                    # Filled in by the wrapper.
-                   start_n, start_m, num_steps,  #
-                   MASK: tl.constexpr):
+                   start_n, start_m, num_steps):
     offs_m = start_m + tl.arange(0, BLOCK_M1)
     offs_n = start_n + tl.arange(0, BLOCK_N1)
     offs_k = tl.arange(0, HEAD_DIM)
-    qT_ptrs = Q + offs_m[None, :] * stride_tok + offs_k[:, None] * stride_d
+    qT_ptrs = Q + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
     do_ptrs = DO + offs_m[:, None] * stride_tok + offs_k[None, :] * stride_d
     # BLOCK_N1 must be a multiple of BLOCK_M1, otherwise the code wouldn't work.
     tl.static_assert(BLOCK_N1 % BLOCK_M1 == 0)
     curr_m = start_m
     step_m = BLOCK_M1
     for blk_idx in range(num_steps):
-        qT = tl.load(qT_ptrs, mask=offs_m[None, :] < N_CTX)
+        qT = tl.trans(tl.load(qT_ptrs, mask=offs_m[:, None] < N_CTX))
         # Load m before computing qk to reduce pipeline stall.
         offs_m = curr_m + tl.arange(0, BLOCK_M1)
         m = tl.load(M + offs_m, mask=offs_m < N_CTX)
-        qkT = tl.dot(k, qT)
+        qkT = tl.dot(k, qT) / tl.sqrt(HEAD_DIM)
         #pT = tl.math.exp2(qkT - m[None, :])
         pT = tl.math.exp(qkT - m[None, :])
         # Autoregressive masking.
-        if MASK:
-            mask = (offs_m[None, :] >= offs_n[:, None])
-            pT = tl.where(mask, pT, 0.0)
+        mask = (offs_m[None, :] <= offs_n[:, None])
+        pT = tl.where(mask, pT, 0.0)
         do = tl.load(do_ptrs, mask=offs_m[:, None] < N_CTX)
         # Compute dV.
         ppT = pT
@@ -320,7 +318,7 @@ def _attn_bwd(Q, K, V, sm_scale,  #
     k = tl.load(K + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d, mask=offs_n[:, None] < N_CTX)
     v = tl.load(V + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d, mask=offs_n[:, None] < N_CTX)
 
-    num_steps = BLOCK_N1 // MASK_BLOCK_M1
+    num_steps = tl.cdiv(N_CTX, BLOCK_M1)
 
     dk, dv = _attn_bwd_dkdv(dk, dv,  #
                             Q, k, v, sm_scale,  #
@@ -330,24 +328,7 @@ def _attn_bwd(Q, K, V, sm_scale,  #
                             H, N_CTX,  #
                             MASK_BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
                             start_n, start_m, num_steps,  #
-                            MASK=True  #
                             )
-
-    start_m += num_steps * MASK_BLOCK_M1
-    num_steps = (N_CTX - start_m) // BLOCK_M1
-
-    # Compute dK and dV for non-masked blocks.
-    dk, dv = _attn_bwd_dkdv(  #
-        dk, dv,  #
-        Q, k, v, sm_scale,  #
-        DO,  #
-        M, D,  #
-        stride_tok, stride_d,  #
-        H, N_CTX,  #
-        BLOCK_M1, BLOCK_N1, HEAD_DIM,  #
-        start_n, start_m, num_steps,  #
-        MASK=False  #
-    )
 
     dv_ptrs = DV + offs_n[:, None] * stride_tok + offs_k[None, :] * stride_d
     tl.store(dv_ptrs, dv, mask=offs_n[:, None] < N_CTX)
@@ -475,6 +456,7 @@ class _attention(torch.autograd.Function):
             BATCH, N_HEAD, N_CTX,  #
             BLOCK_M=PRE_BLOCK, HEAD_DIM=ctx.HEAD_DIM  #
         )
+        print(f'triton d: {delta.sum()}')
         grid = (triton.cdiv(N_CTX, BLOCK_N1), 1, BATCH * N_HEAD)
         scale = 1.0 / ctx.HEAD_DIM**0.5
         arg_k = k
@@ -511,7 +493,7 @@ if __name__ == '__main__':
     k = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
     v = torch.randn((BATCH, H, N_CTX, HEAD_DIM), dtype=dtype, device=device, requires_grad=True)
     sm_scale = 1.3
-    causal=False
+    causal = True
     fn = lambda: attention(q, k, v, causal, sm_scale)
     if mode == "bwd":
         o = fn()